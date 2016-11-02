68d58120cc4d8877dc4238bd053532879054a9b7
xipi000 <xipi000@126.com>
2016-11-02 17:07:09 +0800

    update

--- RuiLin_Plugin.js
+++ RuiLin_Plugin.js
@@ -1,3 +1,4 @@
+cordova.define("RuiLin_Plugin.RuiLin_Plugin", function(require, exports, module) {
 /*
  * 自定义插件的js模块文件
  * 这里仿照官方的方法将Cordova.exec()包装成插件.插件方法()的格式
@@ -10,7 +11,7 @@ var exec = require('cordova/exec');
 var RuiLin_Plugin = {
     
     verifyPassword: function(sendMsg, onSuccessFuction, onFailFuction) {
-    
+
         return exec(onSuccessFuction, onFailFuction, 'RuiLin_Plugin', 'verifyPassword', sendMsg);
     
     },
@@ -18,10 +19,23 @@ var RuiLin_Plugin = {
     getRealAddress: function(sendLoc, onSuccess, onFail) {
     
         return exec(onSuccess, onFail, 'RuiLin_Plugin', 'getRealAddress', sendLoc);
+    },
+      
+    getSmsCode: function(sendMsg, onSuccessFuction, onFailFuction) {
+       
+        return exec(onSuccessFuction, onFailFuction, 'RuiLin_Plugin', 'getSmsCode', sendMsg);
+       
+    },
+       
+       
+    smsLogin: function(sendMsg, onSuccessFuction, onFailFuction) {
+       
+        return exec(onSuccessFuction, onFailFuction, 'RuiLin_Plugin', 'smsLogin', sendMsg);
     }
-    
 };
 
 
 module.exports = RuiLin_Plugin;
 
+
+});
